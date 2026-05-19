991f8f3222f9da56300a10e0585bcdc65622cd9b
nysthretha <ahmetatsiz@gmail.com>
2026-05-19 12:10:34 +0300

    different fix for log injection

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"database/sql"
 	"embed"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/go-chi/chi"
@@ -34,6 +36,10 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT environment variable is not set")
+	}
+	portNum, err := strconv.Atoi(port)
+	if err != nil {
+		log.Fatalf("PORT environment variable is not a valid number: %v", err)
 	}
 
 	apiCfg := apiConfig{}
@@ -90,11 +96,11 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:              ":" + port,
+		Addr:              fmt.Sprintf(":%d", portNum),
 		Handler:           router,
 		ReadHeaderTimeout: 5 * time.Second,
 	}
 
-	log.Printf("Serving on port: %q\n", port)
+	log.Printf("Serving on port: %d\n", portNum)
 	log.Fatal(srv.ListenAndServe())
 }
